0f99329075b5fb1ddbd42504bbc24fae7ff00250
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-02-06 17:33:07 +0200

    donations cors

--- src/server.js
+++ src/server.js
@@ -12,21 +12,21 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors({
+  origin: ['http://themusicstop.app', 'http://localhost:3000']
+}));
 
-const whitelist = ['https://themusicstop.app', 'http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-  preflightContinue: false
-}
-app.use(cors(corsOptions));
+// const whitelist = ['http://themusicstop.app', 'http://localhost:3000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+// }
+// app.use(cors(corsOptions));
 
 //const JWT_SECRET = require('crypto').randomBytes(64).toString('hex');
 //console.log('secret: '+JWT_SECRET);
