6b08af306d06528fb1ef161f802aa41faf45e09b
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-02-06 08:46:49 +0200

    added donations

--- src/server.js
+++ src/server.js
@@ -14,6 +14,27 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 const app = express();
 
 app.use(express.json());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://themusicstop.app');
+
+    // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //
+    // // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //
+    // // Set to true if you need the website to include cookies in the requests sent
+    // // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(cors());
 
 //const JWT_SECRET = require('crypto').randomBytes(64).toString('hex');
